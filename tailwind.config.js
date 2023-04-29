6ebe0ddb9e85b950659a5f58c5ade7f129365154
Abmael <abmael.cont@gmail.com>
2023-04-29 15:43:28 -0300

    chore: basic theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        heading: ["playfair display", "serif"],
+        base: ["raleway", "sans-serif"],
+      },
+      textcolor: {
+        accent: "#a80b0b",
+        secondary: "#900909",
+        tertiary: "#f5f0f0",
+      },
+      backgroundcolor: {
+        100: "#f5f0f0",
+        200: "#f2ebeb",
+        300: "#f22e2e",
+        400: "#900909",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [],
 }
