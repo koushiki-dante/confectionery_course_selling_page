cbd2a8ef92d6897c653f222a5476ca6a2e78443a
Abmael <abmael.cont@gmail.com>
2023-05-02 17:00:46 -0300

    feat: hero section mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     extend: {
-      fontfamily: {
-        heading: ["playfair display", "serif"],
-        base: ["raleway", "sans-serif"],
+      fontFamily: {
+        heading: ["Playfair Display", "serif"],
+        base: ["Raleway", "sans-serif"],
       },
-      textcolor: {
+      textColor: {
         accent: "#a80b0b",
         secondary: "#900909",
         tertiary: "#f5f0f0",
       },
-      backgroundcolor: {
+      backgroundColor: {
         100: "#f5f0f0",
         200: "#f2ebeb",
         300: "#f22e2e",
         400: "#900909",
       },
+      maxWidth: {
+        page: '1280px'
+      },
+      padding: {
+        'screen-edge': '2rem'
+      },
+      gridTemplateColumns: {
+        'nav': '1fr max-content max-content',
+        'content': '0.1fr 1fr 1fr 0.1fr',
+        '2content': '0.15fr 1fr 1fr 0.15fr',
+      },
       screens: {
         xs: "480px",
+        "2lg": "1072px",
+        "3lg": "1120px",
+        "4lg": "1200px",
       },
     },
   },
